1588c31a409ac093f5f853ee18125d66bd4f367c
unknown <yeahmatte@gmail.com>
2020-05-09 21:49:57 +0200

    added cert config

--- install_tools/start.py
+++ install_tools/start.py
@@ -22,15 +22,26 @@ from download_config import get_file
 for file in install_toml:
     get_file(install_toml[file]['file_path'], install_toml[file]['file_id'])
 
-# read_compose_config
-# replace
-# Read in the file
-with open('file.txt', 'r') as file :
-  filedata = file.read()
 
-# Replace the target string
-filedata = filedata.replace('ram', 'abcd')
+cert_config_path = './install_tools/cert_config.toml'
+if os.path.isfile(cert_config_path):
+    with open(cert_config_path) as f:
+        cert_config = toml.load(f)
+        f.close()
+
+with open('docker-compose-template.yml', 'r') as file :
+  filedata = file.read()
+  file.close()
+
+if "domain" in cert_config:
+    if "domain_name" in cert_config["domain"]:
+        # Replace the target string
+        filedata = filedata.replace('YOUR_DOMAIN_HERE', cert_config["domain"]["domain_name"])
+    if "domain_email" in cert_config["domain"]:
+        # Replace the target string
+        filedata = filedata.replace('YOUR_DOMAIN_EMAIL', cert_config["domain"]["domain_email"])
 
 # Write the file out again
-with open('file.txt', 'w') as file:
+with open('docker-compose.yml', 'w') as file:
   file.write(filedata)
+  file.close()
